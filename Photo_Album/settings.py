b473a3078c9189daa57968a0e8a201fc8427d6cf
EC2 Default User <ec2-user@ip-172-31-20-84.eu-west-1.compute.internal>
2024-04-21 00:02:01 +0000

    postgres updated db

--- Photo_Album/settings.py
+++ Photo_Album/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Photo_Album.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photogallerydb',
+        'USER': 'postgres',
+        'PASSWORD': 'ASabc123',
+        'HOST': 'photogallerydb.cp82auyeyecg.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
