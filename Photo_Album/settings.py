bd47f14935ebf568989afe048068658af9e636e5
EC2 Default User <ec2-user@ip-172-31-20-84.eu-west-1.compute.internal>
2024-04-21 00:14:50 +0000

    admin updated

--- Photo_Album/settings.py
+++ Photo_Album/settings.py
@@ -109,15 +109,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
 
+# USE_I18N = True
+
+# USE_L10N = True
+
+# USE_TZ = True
+
+LANGUAGE_CODE = 'en-ie'
+ 
+TIME_ZONE = 'Europe/Dublin'
+ 
 USE_I18N = True
-
-USE_L10N = True
-
+ 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
